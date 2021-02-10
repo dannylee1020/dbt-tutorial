68a5d4b9b26207ae1b8095a930c47ec1cee4b1df
dannylee1020 <dannylee1020@gmail.com>
2021-02-09 20:14:42 -0800

    adding primary key with dbt_utils

---
+++ models/core/customers_daily_summary.sql
@@ -0,0 +1,10 @@
+# number of orders per customer on a given day
+
+select
+  -- generating primary key column
+  {{ dbt_utils.surrogate_key['customer_id', 'order_date']}} as id
+  customer_id,
+  order_date,
+  count(distinct order_id) as order_count 
+from {{ ref('stg_orders') }}
+group by 1,2,3
